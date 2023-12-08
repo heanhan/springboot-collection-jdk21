205295a63fef787109b5e63919ebf40837c12be7
zhaojh <1763124707@qq.com>
2023-12-08 13:26:20 +0800

    项目提交信息 提交人：赵建华 提交内容： 1、项目中的基础依赖的提交，解决打包的报错等问题。打包插件的修复

--- redis-lettuce/src/main/java/org/example/redis/lettuce/config/RedisTemplateConfig.java
+++ redis-lettuce/src/main/java/org/example/redis/lettuce/config/RedisTemplateConfig.java
@@ -1,30 +1,71 @@
 package org.example.redis.lettuce.config;
 
 
+import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.net.UnknownHostException;
 
 @Configuration
 public class RedisTemplateConfig {
 
 
-    /**
-     * 自定义化 redis 的key和value的序列化
-     * @param redisConnectionFactory
-     * @return
-     */
     @Bean
-    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
-        redisTemplate.setConnectionFactory(redisConnectionFactory);
-        redisTemplate.setKeySerializer(new StringRedisSerializer());
-        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-        redisTemplate.setHashKeySerializer(new StringRedisSerializer());
-        redisTemplate.setHashValueSerializer(new GenericJackson2JsonRedisSerializer());
-        return redisTemplate;
+    @ConditionalOnMissingBean(name = "redisTemplate")
+
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory factory)
+            throws UnknownHostException {
+        // 默认的 RedisTemplate 没有过多的设置，redis 对象都是需要序列化！
+        // 两个泛型都是 Object, Object 的类型，我们后使用需要强制转换 <String, Object>
+        // 我们为了自己开发方便，一般直接使用 <String, Object>
+        RedisTemplate<String, Object> template = new RedisTemplate<String, Object>();
+        template.setConnectionFactory(factory);
+
+        // Json序列化配置
+        FastJsonRedisSerializer fastJsonRedisSerializer = new FastJsonRedisSerializer(Object.class);
+        ObjectMapper objectMapper=new ObjectMapper();
+        objectMapper.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+        objectMapper.activateDefaultTyping(LaissezFaireSubTypeValidator.instance, ObjectMapper.DefaultTyping.NON_FINAL, JsonTypeInfo.As.PROPERTY);
+//        fastJsonRedisSerializer.setObjectMapper(objectMapper);
+
+//        ObjectMapper om = new ObjectMapper();
+//        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
+//        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+//        fastJsonRedisSerializer.setObjectMapper(om);
+        // String 的序列化
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        // key采用String的序列化方式
+        template.setKeySerializer(stringRedisSerializer);
+        // hash的key也采用String的序列化方式
+        template.setHashKeySerializer(stringRedisSerializer);
+        // value序列化方式采用jackson
+        template.setValueSerializer(fastJsonRedisSerializer);
+        // hash的value序列化方式采用jackson
+        template.setHashValueSerializer(fastJsonRedisSerializer);
+        template.afterPropertiesSet();
+        return template;
+
     }
+
+    @Bean
+    @ConditionalOnMissingBean
+    // 由于 String 是redis中最常使用的类型，所以说单独提出来了一个bean！
+    public StringRedisTemplate stringRedisTemplate(RedisConnectionFactory redisConnectionFactory)
+            throws UnknownHostException {
+        StringRedisTemplate template = new StringRedisTemplate();
+        template.setConnectionFactory(redisConnectionFactory);
+        return template;
+    }
+
 }
